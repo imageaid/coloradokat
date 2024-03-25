c3efa9968b880d2a9f8ba19504b3af5c791c4257
Craig Kaminsky <imageaid@gmail.com>
2024-03-25 06:49:51 -0600

    undo caching for now

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    @services = Rails.cache.fetch('services', expires_in: 1.day) do
-      Service.where(active: true).order("RANDOM()").limit(6)
-    end
-    photo_set = Rails.cache.fetch('photo_set', expires_in: 1.day) do
-      PhotoSet.where(active: true).order("RANDOM()").first
-    end
+    # @services = Rails.cache.fetch('services', expires_in: 1.day) do
+    #   Service.where(active: true).order("RANDOM()").limit(6)
+    # end
+    # photo_set = Rails.cache.fetch('photo_set', expires_in: 1.day) do
+    #   PhotoSet.where(active: true).order("RANDOM()").first
+    # end
+    @services = Service.where(active: true).order("RANDOM()").limit(6)
+    photo_set = PhotoSet.where(active: true).order("RANDOM()").first
     @photos = photo_set.present? ? photo_set.photos.shuffle : []
     render layout: "landing"
   end
